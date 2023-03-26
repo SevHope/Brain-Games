68dbc3d6de8d5946860d08fdc1fbbad98ec1bb18
Nadezhda Sevastianova <s.hope@bk.ru>
2023-03-26 13:36:00 +0300

    add even.js add package

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 import userName from '../cli';
 
-const miss = 'Lets try again, ${userName}!';
+const miss = ''yes' is wrong answer ;(. Correct answer was 'no'. Lets try again, `${userName}!`';
 const evenNumber = (number) => {
     console.log('Answer "yes" if the number is even, otherwise answer "no".')
     for (let score = 0; score <= 4; score += 1) {
