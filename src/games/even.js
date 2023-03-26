722249b86fceab8c2d5e04f105733ec76348f2a0
Nadezhda Sevastianova <s.hope@bk.ru>
2023-03-26 13:32:53 +0300

    add even.js

---
+++ src/games/even.js
@@ -0,0 +1,18 @@
+import readlineSync from 'readline-sync';
+import userName from '../cli';
+
+const miss = 'Lets try again, ${userName}!';
+const evenNumber = (number) => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".')
+    for (let score = 0; score <= 4; score += 1) {
+const answer = readlineSync.question()
+    if (number % 2 === 0 || answer === 'yes') {
+        console.log("Correct!")
+    }
+    if (number % 2 !== 0 || answer === 'no') {
+        console.log("Correct!")
+    }
+    else return miss;
+    }
+};
+export default evenNumber;
