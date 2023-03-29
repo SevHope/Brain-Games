3eef24a55cc8b4b7bf3d47f2e38ef918d0e3a87b
Nadezhda Sevastianova <s.hope@bk.ru>
2023-03-29 17:50:38 +0300

    update brain-gcd, gsd, package-lock, package

---
+++ src/games/gcd.js
@@ -0,0 +1,30 @@
+import randomNumber from '../randomNumber.js';
+import startGame from '../index.js';
+
+//Games'task
+const gameTask = ('Find the greatest common divisor of given numbers.');
+
+//Meaning of the game
+const brainGcd = (number1, number2) => {
+    while (number1 != number2) {
+        if (number1 > number2) {
+        number1 = number1 - number2;}
+        else {
+        number2 = number2 - number1;
+    };
+    };
+    let result = number1;
+    return result;
+};
+
+//Game's rules
+const gameRules = () => {
+    const number1 = randomNumber(1, 100);
+    const number2 = randomNumber(1, 100);
+    const question = `${number1} ${number2}`;
+    const result = brainGcd(number1, number2);
+    return [question, result];
+};
+export default () => {
+    startGame(gameTask, gameRules);
+  };
