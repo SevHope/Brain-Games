7ee31c9404f31d430fb218deb684debf9f0f8502
Nadezhda Sevastianova <s.hope@bk.ru>
2023-03-30 17:39:53 +0300

    update brain-prime, prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import primeNumber from "../src/games/prime.js";
+
+primeNumber();
---
+++ src/games/prime.js
@@ -0,0 +1,33 @@
+import randomNumber from '../randomNumber.js';
+import startGame from '../index.js';
+
+//Games'task
+export const gameTask = ('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+//Meaning of the game
+const primeNumber = (number) => {
+    if (number === 1) {
+      return false;
+    }
+    else if(number === 2) {
+      return true;
+    } else {
+      for (let i = 2; i < number; i += 1) {
+        if (number % i === 0) {
+          return false;
+        }
+      }
+      return true;  
+    }
+  };
+
+//Game's rules
+const gameRules = () => {
+    const number = randomNumber(1, 100);
+    const truAnswer = primeNumber(number) ? 'yes' : 'no';
+    return [number, truAnswer];
+};
+
+export default () => {
+    startGame(gameTask, gameRules);
+  };
